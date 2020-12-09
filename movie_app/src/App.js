d6f37e145fa1f00c41da092f74586220cb6af27f
prograsshopper <tjalwled12@gmail.com>
2020-12-09 16:10:32 +0900

    add and run with custom component 'Potato'

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,5 +1,12 @@
+import Potato from './Potato';
+
 function App() {
-  return <div className="App" />;  
+  return (
+    <div>
+      <h1>Hello!</h1>
+      <Potato />
+    </div>
+  );
 };
 
 export default App;

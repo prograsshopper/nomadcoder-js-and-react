b81b85b87c69b3d2310507c3f189356ac5ee3e73
prograsshopper <tjalwled12@gmail.com>
2020-12-29 17:58:39 +0900

    map Recap

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -4,23 +4,30 @@ function Food({ name ,picture}) {
 return (
     <div>
     <h2>I like {name}!</h2>
-    <img src={picture} alt="" />
+    <img src={picture} alt={name} />
     </div>
   );
 }
 
+function renderFood(dish){
+  return <Food name={dish.name} picture={dish.image} />;
+}
+
 const foodILike = [
   {
+    id: 1,
     name: "Kimchi",
     image:
       "https://www.maangchi.com/wp-content/uploads/2014/06/whole-cabbage-kimchi.jpg"
   },
   {
+    id: 2,
     name: "Steak",
     image:
       "https://www.thespruceeats.com/thmb/hl4lkmdLO7tj1eDCsGbakfk97Co=/3088x2055/filters:fill(auto,1)/marinated-top-round-steak-3060302-hero-02-ed071d5d7e584bea82857112aa734a94.jpg"
   },
   {
+    id: 3,
     name: "Croissant",
     image:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcKAxPGQ8mOPnE_LwLiMNhtD4x_y9q9Dm9uQ&usqp=CAU"
@@ -30,7 +37,8 @@ const foodILike = [
 function App() {
   return (
     <div>
-      {foodILike.map(dish => <Food name={dish.name} picture={dish.image} />)}
+      {foodILike.map(dish => <Food key={dish.id} name={dish.name} picture={dish.image} />)}
+      {/* {foodILike.map(renderFood)} */}
     </div>
   );
 };

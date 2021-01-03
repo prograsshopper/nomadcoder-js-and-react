86b0090920b795643974e8bfb79b5e9346af765c
prograsshopper <tjalwled12@gmail.com>
2021-01-03 16:10:51 +0900

    Rendering the Movies

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component{
     state = {
@@ -16,10 +17,20 @@ class App extends React.Component{
         this.getMovies();
     }
     render(){
-        const { isLoading } = this.state
+        const { isLoading, movies } = this.state
         return (
             <div>
-                {isLoading? "Loading...": "We are ready"}
+                {isLoading? "Loading...": movies.map(movie=>(
+                    
+                     <Movie 
+                        key={movie.id} 
+                        id={movie.id} 
+                        year={movie.year} 
+                        title={movie.title} 
+                        summary={movie.summary} 
+                        poster={movie.medium_cover_image} 
+                    />
+                ))}
             </div>
         );
   }

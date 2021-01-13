7092aab636f97ac3daa0e62ca3a4833c6d5ad90a
prograsshopper <tjalwled12@gmail.com>
2021-01-14 02:19:38 +0900

    VanillaJS Challenge: day.2 ~2.4

--- vanillaJS-in-2-weeks/index.js
+++ vanillaJS-in-2-weeks/index.js
@@ -1,1 +1,17 @@
-console.log("I'm working. I'm JS, and I am beautiful!");
+// const title = document.getElementById("title");
+const title = document.querySelector("#title");
+title.innerHTML = "Hello fro JS!";
+title.style.color = 'blue';
+document.title = "I own you now";
+
+// function handleResize(){
+//     console.log("I have been resized");
+// }
+
+// window.addEventListener("resize", handleResize);
+
+function handleClick(){
+    title.style.color = 'white';
+}
+
+title.addEventListener("click", handleClick)

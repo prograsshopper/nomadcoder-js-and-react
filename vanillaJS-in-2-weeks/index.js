942ca6f9251475db0566f810234ae0f62af3de34
prograsshopper <tjalwled12@gmail.com>
2021-01-15 01:34:38 +0900

    VanillaJS Challenge: day.3 ~2.7

--- vanillaJS-in-2-weeks/index.js
+++ vanillaJS-in-2-weeks/index.js
@@ -1,30 +1,17 @@
 const title = document.querySelector("#title");
 
-const BASE_COLOR = "white";
-const OTHER_COLOR = "blue";
+const CLICKED_CLASS = "clicked";
 
-title.innerHTML = "Hello fro JS!";
-title.style.color = 'blue';
-document.title = "I own you now";
-
-function handleClick(){
-    const currentColor = title.style.color;
-
-    if (currentColor == BASE_COLOR){
-        title.style.color = OTHER_COLOR;
+function handleClick() {
+    const hasClass = title.classList.contains(CLICKED_CLASS);
+    if (!hasClass){
+        title.classList.add(CLICKED_CLASS);
     } else {
-        title.style.color = BASE_COLOR;
+        title.classList.remove(CLICKED_CLASS);
     }
 }
 
 function init(){
-    title.style.color = BASE_COLOR;
     title.addEventListener("click", handleClick);
 }
 init();
-
-function handleOffline(){
-    console.log('Tralala');
-}
-
-window.addEventListener("offline", handleOffline);
